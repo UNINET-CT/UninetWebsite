a2609d85e06bce2395465038c3f0d20fa0707a44
Kevin Freyberg <kevinfreyberg@pop-os.localdomain>
2024-11-07 23:53:11 -0500

    fix

--- src/DemoPage.js
+++ src/DemoPage.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Box, Typography, Container, useMediaQuery, Divider } from '@mui/material';
 import Navbar from './Navbar';
 import Footer from './Footer';
